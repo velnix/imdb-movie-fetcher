924cee322dab43cd9b796f354598b810cdbe915e
Mikhail Fedotov <anyremote@mail.ru>
2012-12-17 14:20:04 +0400

    add -p and -m options, solved some issued with parsing

--- imdbmoviefetcher/imdb-mf.sh
+++ imdbmoviefetcher/imdb-mf.sh
@@ -15,9 +15,12 @@ declare YEAR
 declare RATING
 declare PLOT
 declare MOVIE
+declare POSTER=0
+declare PARSEABLE=0
 declare DIRECTOR
 declare GENRE
 declare CAST
+declare POSTERURL
 declare -rx TMPFILE=/tmp/imdbmoviefetcher.$$
 declare -rx PLOTFILE=/tmp/plot.$$.html
 declare -rx LYNX="/usr/bin/lynx"
@@ -27,26 +30,28 @@ declare -rx EGREP="/bin/egrep"
 declare -rx GREP="/bin/grep"
 declare -rx UNIQ="/usr/bin/uniq"
 declare -rx HEAD="/usr/bin/head"
-declare -rx SORT="/usr/bin/sort"
+declare -rx SORT=`which sort`
 declare SWITCH
-declare -r OPTSTRING=":hvt:"
+declare -r OPTSTRING=":hvpmt:"
 declare -i INEEDCOLOR
 declare TITLECOLOR="White"
 declare DATACOLOR="White"
 declare LINKCOLOR="White"
 declare CONFFILE="/etc/imdb-mf.conf"
+declare COLOR_START=""
+declare COLOR_END=""
+INEEDCOLOR=0
 #Get color for colorised output
 if [ -f $HOME/.imdb-mf.conf ]
 then
 CONFFILE=$HOME/.imdb-mf.conf 
 fi
-if [ ! -f $CONFFILE ]
+if [ -f $CONFFILE ]
 then 
-echo "Not able to see the conf file at $CONFFILE. Create it!!"
-exit 192
-fi
-INEEDCOLOR=0
+declare COLOR_START='\033['
+declare COLOR_END='\033[0m'
 . $CONFFILE
+fi
 getcolor()
 {
 COMPONENT=$1
@@ -111,12 +116,9 @@ getcolor $TITLECOLOR TITLECOLOR
 getcolor $DATACOLOR DATACOLOR
 getcolor $LINKCOLOR LINKCOLOR
 else
-TITLECOLOR="White"
-DATACOLOR="White"
-LINKCOLOR="White"
-getcolor $TITLECOLOR TITLECOLOR
-getcolor $DATACOLOR DATACOLOR
-getcolor $LINKCOLOR LINKCOLOR
+TITLECOLOR=""
+DATACOLOR=""
+LINKCOLOR=""
 fi
 #End of colorse function
 #check the input arguments/ parametrs
@@ -133,6 +135,8 @@ h) cat << EOF
    -v : gives the version of the script
    -h : shows the help page
    -t [arg] : Pass the movie title as argument. It is recommended to quote the name as shown in the example below 
+   -p : Download movie poster 
+   -m : Output in machine-readable format, easy for subsequent parsing 
 
    Example : 
    $SCRIPT -t "startship troopers"
@@ -143,6 +147,11 @@ EOF
    exit 0
    ;;
 t) MOVIE="$OPTARG"
+   ;;
+p) POSTER="1"
+   ;;
+m) PARSEABLE="1"
+   INEEDCOLOR="0"
    ;;
 v) printf "IMDB movie fetcher version %s.\nBugs, Feature requests etc : %s\n" "$VERSION" "$MYSITE"
    exit 0
@@ -174,14 +183,11 @@ then
 printf "This script is written for bash. Please run this under bash\n" >&2
 exit 192
 fi
+# do sancheck only for utilities which absent in coreutils package
 sancheck $LYNX
-sancheck $CAT
 sancheck $EGREP
 sancheck $GREP
-sancheck $UNIQ
-sancheck $HEAD
 sancheck $SED
-sancheck $SORT
 #sanity check finished
 #replace special characters in the movie name argument
 MOVIE=`echo $MOVIE | sed -r 's/  */\+/g;s/\&/%26/g;s/\++$//g'`
@@ -196,7 +202,7 @@ fi
 #Get IMDB URL for the film
 URL=`$EGREP -o "http://www.imdb.com/title/tt[0-9]*/" $TMPFILE | $HEAD -1`
 #Get similar titles
-SIMILARTITLES=`egrep -o "<a[^>]+>[^<]*(<em>[^<|^(imdb)]+</em>)*[^<]*<em>[^<]*</em>[^\(|^<]*\([0-9]+\)[^-]+-[^<]*<em>IMDb</em>" $TMPFILE | grep "www.imdb.com" | sort | uniq | sed 's/- <em>IMDb<\/em>//g' | sed 's/<[^>]*>//g'`
+SIMILARTITLES=`egrep -o "<a[^>]+>[^<]*(<b>[^<|^(imdb)]+</b>)*[^<]*<b>[^<]*</b>[^\(|^<]*\([0-9]+\)[^-]+-[^<]*<b>IMDb</b>" $TMPFILE | grep "www.imdb.com" | sort | uniq | sed 's/- <b>IMDb<\/b>//g' | sed 's/<[^>]*>//g'`
 #get the details from movie page
 $LYNX --source ${URL} > $TMPFILE;
 if [ $? -ne 0 ]
@@ -205,31 +211,60 @@ printf "Connection to site failed...Please check your internet connection\n"
 exit 192
 fi
 #extract data
-YEAR=`$CAT $TMPFILE | $SED -n '/<h1.*>/,/<\/h1>/p' | $SED '1d;$d;/^$/d;s/<[^>]*>//g;s/(//g;s/)//g' | $EGREP -o "[0-9][0-9][0-9][0-9]"`
-TITLE=`$CAT $TMPFILE | $SED -n '/<h1.*>/,/<\/h1>/p' | $SED '1d;$d;/^$/d;s/<[^>]*>//g;s/(//g;s/)//g' | $HEAD -1 | $SED "s/\&#x27\;/\'/g" | $LYNX -stdin -dump -nomargins`
+YEAR=`$CAT $TMPFILE | $SED -n '/<h1.*>/,/<\/h1>/p' | $SED -n '/<span.*>/,/<\/span>/p' | $SED '/^$/d;s/<[^>]*>//g;s/(//g;s/)//g' | $SED 's/&ndash;/ - /g'| $EGREP -o "[0-9][0-9][0-9][0-9]( - [0-9][0-9][0-9][0-9])*"`
+TITLE=`$CAT $TMPFILE | $SED -n '/<h1.*>/,/<\/h1>/p' | $SED '1d;$d;/^$/d;s/<[^>]*>//g;s/(//g;s/)//g' | $HEAD -1 | $SED "s/\&#x27\;/\'/g"`
+POSTERURL=`grep "Poster" $TMPFILE -B4|grep -o http.*\.jpg`
+if [ $POSTER -eq 1 ] 
+then
+POSTERFILE=`echo $TITLE | $SED "s/ /_/g"`
+$LYNX -connect_timeout=10 --source $POSTERURL > ${POSTERFILE}.jpg 2> /dev/null
+fi
 #Get the plot in a html file
 $SED -n '/<h1.*>/,/<\/p>/p' $TMPFILE | $SED -n '/<p>/,/<\/p>/{ s/<[^>]*>//g;p;}' | $SED 's/See full summary.*//g' > $PLOTFILE 
 PLOT=`lynx --dump $PLOTFILE | sed 's/^  *//g'`
 RATING=`$SED -n '/<span itemprop="ratingValue">/{ s/<[^>]*>//g;p;}' $TMPFILE  | $EGREP -o "[0-9]+\.[0-9]+/[0-9]+" | tail -1`;
-DIRECTOR=`$SED -n '/ *Director[s]*:.*/,/ *Writer[s]*:.*/{p;}' $TMPFILE | $SED '1d;$d' | tr '\n' ' ' | $SED 's/<[^>]*>//g' | $SED 's/^ *//g;s/ *$//g'`
+DIRECTOR=`$SED -n '/ *Director[s]*:.*/,/ *Writer[s]*:.*/{p;}' $TMPFILE | $SED -n '/ *Director[s]*:.*/,/ *Star[s]*:.*/{p;}' | $SED '1d;$d' | tr '\n' ' ' | $SED 's/<[^>]*>//g;s/^ *//g;s/ *$//g;s/&nbsp;&raquo;//g'`
 GENRE=`$SED -n '/genre/p' $TMPFILE | $EGREP -o '<a  *.*href="/genre/[a-zA-Z][a-z]*"[^>]*>[^<]*</a>' | $SED 's/<[^>]*>//g;s/&nbsp\;//g;s/ *Genres *[0-9][0-9]* *min-//g;s/|/,/g' | $SED 's/[gG]enres//g;s/^  *//g' | $UNIQ | $HEAD -1`
-CAST=`$SED -n '/.*Star[s]*:.*/,/<\/div>/{ s/<[^>]*>//g;s/Stars://g;p }' $TMPFILE | $SED -n '/^ *$/d;p' | tr '\n' ' ' | $SED 's/<[^>]*>//g'`
+CAST=`$SED -n '/.*Star[s]*:.*/,/<\/div>/{ s/<[^>]*>//g;s/Stars://g;p }' $TMPFILE | grep -v "See full cast and crew" | $SED -n '/^ *$/d;p' | tr '\n' ' ' | $SED 's/<[^>]*>//g;s/|//g'`
+if [ $INEEDCOLOR -eq 1 ] 
+then
+TITLE_COLOR_TAG="${TITLECOLOR_MODE};${TITLECOLOR_CODE}m"
+DATA_COLOR_TAG="${DATACOLOR_MODE};${DATACOLOR_CODE}m"
+LINK_COLOR_TAG="${LINKCOLOR_MODE};${LINKCOLOR_CODE}m"
+else
+TITLE_COLOR_TAG=""
+DATA_COLOR_TAG=""
+LINK_COLOR_TAG=""
+fi
+if [ $PARSEABLE -eq 1 ] 
+then
+PLOT=`echo $PLOT | tr -d '\n'`
+fi
 #print everything
-printf "\n\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}mTitle\033[0m      : \033[${DATACOLOR_MODE};${DATACOLOR_CODE}m$TITLE\033[0m\n"
-printf "\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}mYear\033[0m       : \033[${DATACOLOR_MODE};${DATACOLOR_CODE}m$YEAR\033[0m\n"
-printf "\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}mRating\033[0m     : \033[${DATACOLOR_MODE};${DATACOLOR_CODE}m$RATING\033[0m\n"
-printf "\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}mDirector\033[0m   : \033[${DATACOLOR_MODE};${DATACOLOR_CODE}m$DIRECTOR\033[0m\n"
-printf "\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}mGenres\033[0m     : \033[${DATACOLOR_MODE};${DATACOLOR_CODE}m$GENRE\033[0m\n"
-printf "\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}mCast\033[0m       : \033[${DATACOLOR_MODE};${DATACOLOR_CODE}m$CAST\033[0m\n"
-printf "\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}mPlot\033[0m       :\n\n\033[${DATACOLOR_MODE};${DATACOLOR_CODE}m%s\033[0m\n\n" "$PLOT"
-printf "\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}mIMDB movie URL\033[0m : \033[${LINKCOLOR_MODE};${LINKCOLOR_CODE}m${URL}\033[0m\n\n"
-printf "\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}m==============Other similar Titles=============\033[0m\n\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Title${COLOR_END}      : ${COLOR_START}${DATA_COLOR_TAG}$TITLE${COLOR_END}\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Year${COLOR_END}       : ${COLOR_START}${DATA_COLOR_TAG}$YEAR${COLOR_END}\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Rating${COLOR_END}     : ${COLOR_START}${DATA_COLOR_TAG}$RATING${COLOR_END}\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Director${COLOR_END}   : ${COLOR_START}${DATA_COLOR_TAG}$DIRECTOR${COLOR_END}\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Genres${COLOR_END}     : ${COLOR_START}${DATA_COLOR_TAG}$GENRE${COLOR_END}\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Cast${COLOR_END}       : ${COLOR_START}${DATA_COLOR_TAG}$CAST${COLOR_END}\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Plot${COLOR_END}       :${COLOR_START}${DATA_COLOR_TAG}%s${COLOR_END}\n" "$PLOT"
+printf "${COLOR_START}${TITLE_COLOR_TAG}IMDB movie URL${COLOR_END} : ${COLOR_START}${LINK_COLOR_TAG}${URL}${COLOR_END}\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Poster URL${COLOR_END} : ${COLOR_START}${LINK_COLOR_TAG}${POSTERURL}${COLOR_END}\n"
+if [ $PARSEABLE -eq 0 ] 
+then
+printf "${COLOR_START}${TITLE_COLOR_TAG}\n==============Other similar Titles=============${COLOR_END}\n\n"
 if [ -z "$SIMILARTITLES" ]
 then
 SIMILARTITLES="Nothing interesting here"
 fi
-printf "\033[${DATACOLOR_MODE};${DATACOLOR_CODE}m%s\033[0m\n\n" "$SIMILARTITLES"
-printf "\033[${TITLECOLOR_MODE};${TITLECOLOR_CODE}mUse above key words to know more about them\033[0m\n\n"
+printf "${COLOR_START}${DATA_COLOR_TAG}%s${COLOR_END}\n" "$SIMILARTITLES"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Use above key words to know more about them${COLOR_END}\n"
+else
+if [ ! -z "$SIMILARTITLES" ]
+then
+printf "${COLOR_START}${TITLE_COLOR_TAG}Similarities${COLOR_END} : ${COLOR_START}${LINK_COLOR_TAG}%s${COLOR_END}\n" "$SIMILARTITLES"    
+fi
+fi
 #Done. Now do cleanup
 rm $TMPFILE > /dev/null
 rm $PLOTFILE > /dev/null
