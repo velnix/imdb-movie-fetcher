cae62df99239e782c04b8677327e216e82ce89b9
Unni <unnikrishnan.a@gmail.com>
2015-11-13 19:08:15 +0530

    Version 5 commit. Mainly contributed by Zarko

--- imdbmoviefetcher/imdb-mf.sh
+++ imdbmoviefetcher/imdb-mf.sh
@@ -3,21 +3,26 @@
 #Movie data fetcher developed by Unni <unnikrishnan.a@gmail.com>
 #Code to fetch poster, machine readable output and bug fixes by
 #Mikhail Fedotov <anyremote@mail.ru>
-#http://prj.mutexes.org/projects/imdbmoviefetcher
-#version 4.2
+#Contribution for version 5 from zzarko@gmail.com
+#version 5
 shopt -s -o nounset
 #Global variable declarations
-declare -rx MYSITE="http://prj.mutexes.org/projects/imdbmoviefetcher"
-declare -rx SOURCE_REPO="https://github.com/webofunni/imdb-movie-fetcher"
+declare -rx MYSITE="https://www.velnix.com/projects/"
+declare -rx SOURCE_REPO="https://github.com/velnix/imdb-movie-fetcher"
 declare -rx SCRIPT=${0##*/}
-declare -rx VERSION="4.2"
+declare -rx VERSION="5"
 declare URL
 declare TITLE
+declare OTITLE=""
 declare YEAR
 declare RATING
 declare PLOT
 declare MOVIE
+declare MYEAR=""
 declare POSTER=0
+declare GOOGLE=0
+declare DUCK=0
+declare IMDBID=0
 declare PARSEABLE=0
 declare SIMILAR=1
 declare DIRECTOR
@@ -31,7 +36,7 @@ declare -rx SED="/bin/sed"
 declare -rx EGREP="/bin/egrep"
 declare -rx GREP="/bin/grep"
 declare SWITCH
-declare -r OPTSTRING=":hvpmst:"
+declare -r OPTSTRING=":hvpmsgdt:y:i:"
 declare -i INEEDCOLOR
 declare TITLECOLOR="White"
 declare DATACOLOR="White"
@@ -142,8 +147,12 @@ Options :
    -v : gives the version of the script
    -h : shows the help page
    -t [arg] : Pass the movie title as argument. It is recommended to quote the name as shown in the example below 
+   -y [arg] : Pass the movie year title as argument.
    -p : Download movie poster. Use it with -t option.
    -s : Do not show similar titles.
+   -g : Only search with Google for IMDB ID, don't search IMDB.
+   -d : Only search with DuckDuckGo for IMDB ID, don't search IMDB.
+   -i [arg] : Pass the IMDB ID, skip Google
    -m : Output in machine-readable format, easy for subsequent parsing. Use it with -t option.
 
 Example : 
@@ -156,9 +165,17 @@ EOF
    ;;
 t) MOVIE="$OPTARG"
    ;;
+y) MYEAR="+$OPTARG"
+   ;;
 p) POSTER="1"
    ;;
 s) SIMILAR="0"
+   ;;
+g) GOOGLE="1"
+   ;;
+d) DUCK="1"
+   ;;
+i) IMDBID="$OPTARG"
    ;;
 m) PARSEABLE="1"
    INEEDCOLOR="0"
@@ -175,7 +192,9 @@ v) printf "IMDB movie fetcher version %s.\n\nBugs, Feature requests etc : %s\nSo
 esac
 done
 
-[[ ${MOVIE:-unset} == 'unset' || -z $MOVIE ]] && { printf "Invalid input. Execute %s -h for more information\n" "$SCRIPT";exit 192;}
+if [[ "$IMDBID" == "0" && "$DUCK" == "0" ]]; then
+    [[ ${MOVIE:-unset} == 'unset' || -z $MOVIE ]] && { printf "Invalid input. Execute %s -h for more information\n" "$SCRIPT";exit 192;}
+fi
 
 #functions
 printnofound() {
@@ -205,22 +224,61 @@ sancheck $GREP
 sancheck $SED
 #sanity check finished
 
-#replace special characters in the movie name argument
-MOVIE=`echo $MOVIE | sed -r 's/  */\+/g;s/\&/%26/g;s/\++$//g'`
-#search the title in google
-$LYNX -connect_timeout=10 --source "http://www.google.com/search?hl=en&q=$MOVIE+imdb" > $TMPFILE 2> /dev/null
-#Check if lynx is successful
-if [ $? -ne 0 ]
-then 
-  printf "Connection to site failed...Please check your internet connection\n"
-  exit 192
+if [ "$IMDBID" == "0" ]; then
+    #replace special characters in the movie name argument
+    MOVIE=`echo $MOVIE | sed -r 's/  */\+/g;s/\&/%26/g;s/\++$//g'`
+    if [ "$DUCK" == "0" ]; then
+        #search the title in google
+        #URL="http://www.google.com/search?hl=en&q=\"$MOVIE\"$MYEAR+imdb"
+        URL="https://www.google.com/search?client=ubuntu&channel=fs&q=$MOVIE$MYEAR+imdb&ie=utf-8&oe=utf-8"
+        $LYNX -connect_timeout=10 --source $URL > $TMPFILE 2> /dev/null
+        #Check if lynx is successful
+        if [ $? -ne 0 ]
+        then 
+          printf "Connection to site failed...Please check your internet connection\n"
+          exit 192
+        fi
+        #check if robot detected
+        ROBOT=`$EGREP -o "detected unusual traffic" $TMPFILE | head -1`
+        if [ "$ROBOT" == "detected unusual traffic" ]; then
+            echo "Google detected unusual traffic!!"
+            DUCK="1"
+        else
+            #Get IMDB URL for the film
+            URL=`$EGREP -o "http://www.imdb.com/title/tt[0-9]*/" $TMPFILE | head -1`
+            #Get similar titles
+            SIMILARTITLES=`egrep -o "<a[^>]+>[^<]*(<b>[^<|^(imdb)]+</b>)*[^<]*<b>[^<]*</b>[^\(|^<]*\([0-9]+\)[^-]+-[^<]*<b>IMDb</b>" $TMPFILE | grep "www.imdb.com" | sort | uniq | sed 's/- <b>IMDb<\/b>//g' | sed 's/<[^>]*>//g'`
+        fi
+    fi
+    if [ "$DUCK" == "1" ]; then
+        #search the title in duckduckgo
+        URL="https://duckduckgo.com/?q=$MOVIE$MYEAR+imdb"
+        $LYNX -connect_timeout=10 --source $URL > $TMPFILE 2> /dev/null
+        #Check if lynx is successful
+        if [ $? -ne 0 ]
+        then 
+          printf "Connection to site failed...Please check your internet connection\n"
+          exit 192
+        fi
+        #check if robot detected
+        ROBOT=`$EGREP -o "detected unusual traffic" $TMPFILE | head -1`
+        if [ "$ROBOT" == "detected unusual traffic" ]; then
+            echo "DuckDuck detected unusual traffic"
+            exit 192
+        fi
+        #Get IMDB URL for the film
+        URL=`$EGREP -o "tt[0-9]{4,15}" $TMPFILE | head -1`
+        URL="http://www.imdb.com/title/$URL/"
+    fi
+else
+    URL="http://www.imdb.com/title/$IMDBID/"
+    SIMILARTITLES=""
 fi
 
-
-#Get IMDB URL for the film
-URL=`$EGREP -o "http://www.imdb.com/title/tt[0-9]*/" $TMPFILE | head -1`
-#Get similar titles
-SIMILARTITLES=`egrep -o "<a[^>]+>[^<]*(<b>[^<|^(imdb)]+</b>)*[^<]*<b>[^<]*</b>[^\(|^<]*\([0-9]+\)[^-]+-[^<]*<b>IMDb</b>" $TMPFILE | grep "www.imdb.com" | sort | uniq | sed 's/- <b>IMDb<\/b>//g' | sed 's/<[^>]*>//g'`
+if [ "$GOOGLE" == "1" ]; then
+    echo `echo "$URL" | $EGREP -o "tt[0-9]+"`
+    exit 0
+fi
 
 #get the details from movie page
 $LYNX --source ${URL} > $TMPFILE;
@@ -234,8 +292,17 @@ fi
 #YEAR=`cat $TMPFILE | $SED -n '/<h1.*>/,/<\/h1>/p' | $SED -n '/<span.*>/,/<\/span>/p' | $SED '/^$/d;s/<[^>]*>//g;s/(//g;s/)//g' | $SED 's/&ndash;/ - /g'| $EGREP -o "[0-9][0-9][0-9][0-9]( - [0-9][0-9][0-9][0-9])*"`
 #TITLE=`cat $TMPFILE | $SED -n '/<h1.*>/,/<\/h1>/p' | $SED '1d;$d;/^$/d;s/<[^>]*>//g;s/(//g;s/)//g' | head -1 | $SED "s/\&#x27\;/\'/g"`
 
-YEAR=`cat $TMPFILE | $SED -n '/<h1.*>/,/<\/h1>/p' | $SED -n '/<span class="nobr".*>/,/<\/span>/p' | $SED '/^$/d;s/<[^>]*>//g;s/(//g;s/)//g' | $SED 's/&ndash;/ - /g;/[^"]"/d;s/>//g'|tr -d ' '`
 TITLE=`cat $TMPFILE | $SED -n '/<h1.*>/,/<\/h1>/p' | $SED -n 's/.*itemprop="name">//;s/<.*//p' | head -1 | tr -s ' ' | tr '\n' ' '`
+TITLE=`echo $TITLE`
+YEAR=`cat $TMPFILE | $SED -n '/<h1.*>/,/<\/h1>/p' | $SED -n '/<span class="nobr".*>/,/<\/span>/p' | $SED '/^$/d;s/<[^>]*>//g;s/(//g;s/)//g' | $SED 's/&ndash;/ - /g;/[^"]"/d;s/>//g' | grep -o -E "[0-9]{4}"`
+OTITLE=`cat $TMPFILE | $SED -n '/<span class="title-extra.*>/,/<\/span>/p' | $SED '/^$/d;s/<[^>]*>//g;s/(//g;s/)//g' | $SED -n '/".*"/p' | tr -d '"' | $SED -e 's/^[ \t]*//'`
+#if [[ "$YEAR" == *original* ]]; then
+#    OTITLE=${YEAR#*\"}
+#    OTITLE=${OTITLE%\"*}
+#    YEAR=${YEAR//$'\n'/Y}
+#    YEAR=${YEAR%%Y*}
+#fi
+YEAR=`echo $YEAR|tr -d ' '`
 
 POSTERURL=`grep "Poster" $TMPFILE -A1|grep -o http.*\.jpg|cut -f 1 -d "_"`
 POSTERURL=${POSTERURL}jpg
@@ -258,7 +325,7 @@ PLOT=`lynx --dump $PLOTFILE | sed 's/^  *//g'`
 #CAST=`$SED -n '/.*Star[s]*:.*/,/<\/div>/{ s/<[^>]*>//g;s/Stars://g;p }' $TMPFILE | grep -v "See full cast and crew" | $SED -n '/^ *$/d;p' | tr '\n' ' ' | $SED 's/<[^>]*>//g;s/|//g'`
 
 DIRECTOR=`$SED -n '/ *itemprop="director.*/,/<\/div>/{p;}' $TMPFILE|$SED -n 's/.*itemprop="name">//;s/<.*//p'|uniq|tr -s ' '|tr '\n' ','|sed 's/ ,//g;s/^,//;s/,$//'`
-GENRE=`grep "ref_=tt_ov_inf" $TMPFILE| grep genre|$SED -n 's/.*itemprop="genre">//;s/<.*//p'|uniq|tr '\n' ','|sed 's/^,//;s/,$//'`
+GENRE=`grep "ref_=tt_ov_inf" $TMPFILE | grep genre | $SED -n 's/<a href="\/genre\///p' | $SED -n 's/?ref_=tt_ov_inf"//p' | uniq | tr '\n' ',' | sed 's/^,//;s/,$//'`
 CAST=`$SED -n '/ *tt_ov_st.*/,/tt_ov_st_sm/{p;}' $TMPFILE|$SED -n 's/.*itemprop="name">//;s/<.*//p'|uniq|tr -s ' '|tr '\n' ','|sed 's/ ,//g;s/,,/,/g;s/^,//;s/,$//'`
 
 if [ $INEEDCOLOR -eq 1 ] 
@@ -278,7 +345,10 @@ then
 fi
 
 #print everything
-printf "\n${COLOR_START}${TITLE_COLOR_TAG}Title${COLOR_END}      : ${COLOR_START}${DATA_COLOR_TAG}$TITLE${COLOR_END}\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Title${COLOR_END}      : ${COLOR_START}${DATA_COLOR_TAG}$TITLE${COLOR_END}\n"
+if [ "$OTITLE" != "" ]; then
+    printf "${COLOR_START}${TITLE_COLOR_TAG}Original${COLOR_END}   : ${COLOR_START}${DATA_COLOR_TAG}$OTITLE${COLOR_END}\n"
+fi
 printf "${COLOR_START}${TITLE_COLOR_TAG}Year${COLOR_END}       : ${COLOR_START}${DATA_COLOR_TAG}$YEAR${COLOR_END}\n"
 printf "${COLOR_START}${TITLE_COLOR_TAG}Rating${COLOR_END}     : ${COLOR_START}${DATA_COLOR_TAG}$RATING${COLOR_END}\n"
 printf "${COLOR_START}${TITLE_COLOR_TAG}Director${COLOR_END}   : ${COLOR_START}${DATA_COLOR_TAG}$DIRECTOR${COLOR_END}\n"
@@ -286,7 +356,7 @@ printf "${COLOR_START}${TITLE_COLOR_TAG}Genres${COLOR_END}     : ${COLOR_START}$
 printf "${COLOR_START}${TITLE_COLOR_TAG}Cast${COLOR_END}       : ${COLOR_START}${DATA_COLOR_TAG}$CAST${COLOR_END}\n"
 printf "${COLOR_START}${TITLE_COLOR_TAG}Plot${COLOR_END}       : ${COLOR_START}${DATA_COLOR_TAG}%s${COLOR_END}\n" "$PLOT"
 printf "\n${COLOR_START}${TITLE_COLOR_TAG}IMDB movie URL${COLOR_END} : ${COLOR_START}${LINK_COLOR_TAG}${URL}${COLOR_END}\n"
-printf "${COLOR_START}${TITLE_COLOR_TAG}Poster URL${COLOR_END} : ${COLOR_START}${LINK_COLOR_TAG}${POSTERURL}${COLOR_END}\n"
+printf "${COLOR_START}${TITLE_COLOR_TAG}Poster URL${COLOR_END} : ${COLOR_START}${LINK_COLOR_TAG}${POSTERURL}${COLOR_END}"
 
 if [ $SIMILAR -eq 1 ] 
 then
@@ -313,3 +383,4 @@ fi
 rm $TMPFILE > /dev/null
 rm $PLOTFILE > /dev/null
 exit 0
+
